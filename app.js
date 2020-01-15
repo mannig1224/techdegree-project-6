a95621fbc0bc60e5530fc49c21fdc7cadb356879
manniig24 <egatica51@gmail.com>
2020-01-15 00:12:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express'); // We need to require the express module
 const http = require('http');
 
-
 const app = express(); // starting our express app
 app.set('view engine', 'pug'); // setting to use pug
 app.use('/static', express.static('public')); // setting the public folder at the address /static
